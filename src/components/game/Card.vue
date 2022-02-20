8f467bf73ad9cb7a44db2d0eab048a0e8bda5f24
Pravdin <pravdin.am@mail.ru>
2022-02-20 12:03:31 +0300

    test

---
+++ src/components/game/Card.vue
@@ -0,0 +1,77 @@
+<template>
+  <div v-if="win">
+    <h1 class="win">Победа</h1>
+  </div>
+    <div v-else class="game__wrapper">
+      <div :class="{active: active.includes(i)}" v-for="(image,i) in selectedImage" class="game__wrapper_item" :key="i" @click="showCard(image, i)">
+        <img :src="image.path" alt="" :class="{show: show}"/>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "CardComponent",
+  props: {
+    images: {
+      type: Object,
+      default: () => {}
+    },
+  },
+  watch: {
+    active: {
+      deep: true,
+      handler() {
+        if(this.active.length === this.images.length) {
+          this.win = true
+        }
+      }
+    }
+  },
+  data() {
+    return {
+      active: [],
+      activeCard: [],
+      selectedImage: null,
+      show: true,
+      win: false,
+    }
+  },
+  created() {
+    this.selectedImage = this.shuffle(this.images)
+  },
+  mounted() {
+    setTimeout(() => {
+      this.show = false
+    }, 5000)
+  },
+  methods: {
+    showCard(image, i) {
+      if (this.activeCard.includes(image.id) || this.activeCard.length === 0) {
+        this.active.push(i)
+        this.activeCard.push(image.id)
+      }
+      if(!this.activeCard.includes(image.id)) {
+        this.active = []
+        this.activeCard = []
+      }
+      if(this.activeCard.length === 2) {
+        this.activeCard = []
+      }
+    },
+    shuffle(arr) {
+      for (let i = arr.length - 1; i > 0; --i) {
+        const pos = Math.floor(Math.random() * (i + 1));
+        const t = arr[pos];
+        arr[pos] = arr[i];
+        arr[i] = t;
+      }
+      return arr;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/game/Training.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="game">
+    <CardComponent v-if="start" :images="images"/>
+      <button v-if="!start" @click="startGame">начать</button>
+  </div>
+</template>
+
+<script>
+
+import CardComponent from "@/components/game/Card";
+import {imagesData} from "@/assets/imagesData";
+export default {
+  name: "TrainingComponent",
+  components: {CardComponent},
+  data() {
+    return {
+      images: imagesData,
+      start: false,
+
+    }
+  },
+  methods: {
+    startGame() {
+      this.start = true
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
